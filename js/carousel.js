f229dec1532158a19172e9376361788e8a627901
JOAO GABRIEL FERREIRA DE SOUSA <231.918869@senaicimatec.edu.br>
2025-08-15 17:02:47 -0300

    Aula 01

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,3 @@
-
-
 //carousel
 
 //Array storage class
@@ -9,24 +7,46 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
+    constructor(image, title, uri){
+        this.image = image;
+        this.title = title;
+        this.uri = uri;
+    }
     
       
     static Start(arr){
-        if(arr){
-
-            if(arr.length > 0){
+            if(arr && arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
+                Carousel._arr = arr;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
+            
             
         } else {
-            throw "Method Start need a Array Variable.";
+            // throw "Method Start need a Array Variable.";
+            throw "O método Star precisa receber um array válido!"
         }
     }
 
     static Next(){
-        
+        const carouselElement = document.getElementById("carousel");
+        const titleElement = document.getElementById("carousel-title");
+    
+        if(!carouselElement || !titleElement){
+            console.error("Elemento do carrossel não encontrados!");
+            return;
+        }
+
+        const item = Carousel._arr[Carousel._sequence];
+
+        carouselElement.style.backgroundImage = `url(img/${item.image})`
+        carouselElement.style.backgroundPosition = "center";
+        carouselElement.style.backgroundSize = "cover";
+        carouselElement.style.transition = "background-image 0.5s ease-in-out";
+    
+        titleElement.innerHTML = `<a href= "${item.uri}"> ${item.title}</a>`;
+
+        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
     }
 };
